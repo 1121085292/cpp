9658fa900596344f66801750212c1008b38c1123
tang <1121085292@qq.com>
2023-10-15 12:07:52 +0800

    1015

--- main.cc
+++ main.cc
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <string>
+
+class Entity {
+  public:
+   explicit Entity(const int age) : age_(age) {}
+
+  private:
+    int age_;
+};
+
+struct Vector2 {
+    float x_, y_;
+
+    Vector2(float x, float y)
+        : x_(x), y_(y) {}
+    Vector2 add(const Vector2& other) const {
+        return Vector2(x_ + other.x_, y_ + other.y_);
+    }
+    Vector2 operator+(const Vector2& other) const {
+        // return Vector2(x_ + other.x_, y_ + other.y_);
+        return add(other);
+    }
+    Vector2 multiply(const Vector2& other) const {
+        return Vector2(x_ * other.x_, y_ * other.y_);
+    }
+    Vector2 operator*(const Vector2& other) const {
+        return Vector2(x_ * other.x_, y_ * other.y_);
+    }
+};
 
 int main(){
-    std::cout << "hello" << std::endl;
-    int a = 4;
+    // Entity e(5);
+    Vector2 speed(0.5f, 0.5f);
+    Vector2 position(10.0f, 10.0f);
+    Vector2 powerup(1.1f, 1.1f);
+    // Vector2 result = position.add(speed.multiply(powerup));
+    Vector2 result = position + speed * powerup;
+    std::cout << result.x_ << " " << result.y_ << std::endl;
     return 0;
 }
