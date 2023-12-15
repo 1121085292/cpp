6047507a24c8aba4dd5f34fca515b1d5b093f25a
tang <1121085292@qq.com>
2023-12-15 23:11:34 +0800

    1215

---
+++ multi_sensor_mot/algorithm/kalman_filter/extended_kalman_filter.cc
@@ -0,0 +1,53 @@
+#include "extended_kalman_filter.h"
+
+Eigen::MatrixXd ExtendedKalmanFilter::calculateJacobian()
+{
+  Eigen::MatrixXd Hj(3, 4);
+  // get state parammetes
+  float x = x_(0);
+  float y = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+
+  float c1 = x * x + y * y;
+  float c2 = std::sqrt(c1);
+  float c3 = c1 * c2;
+  float c4 = vx * y - vy * x;
+
+  if(std::fabs(c1) < 0.001){
+    return Hj;
+  }
+
+  Hj << x/c2, y/c2, 0, 0,
+       -y/c1, x/c1, 0, 0,
+       y*c4/c3, -x*c4/c3, x/c2, y/c2;
+  return Hj;
+}
+
+void ExtendedKalmanFilter::predict()
+{
+  x_ = F_ * x_;
+  P_ = F_ * P_ * F_.transpose() + Q_;
+}
+
+void ExtendedKalmanFilter::measurementUpdate(const Eigen::VectorXd& z)
+{
+  // y = z - h(x)
+  double rho = std::sqrt(x_(0) * x_(0) + x_(1) * x_(1));
+  double theta = atan2(x_(1), x_(0));
+  double rho_dot = (x_(0) * x_(2) + x_(1) * x_(3)) / rho;
+  Eigen::VectorXd h(3, 1);
+  h << rho, theta, rho_dot;
+  
+  Eigen::VectorXd y = z - h;
+  // s = h * p * ht + R
+  H_ = calculateJacobian();
+  auto s = H_ * P_ * H_.transpose() + R_;
+  // k = p * ht / s
+  auto k = P_ * H_.transpose() * s.inverse();
+  // x = x + k * y
+  x_ = x_ + k * y;
+  // p = (i - k * h) * p
+  Eigen::MatrixXd I = Eigen::MatrixXd::Identity(x_.size(), x_.size());
+  P_ = (I - k * H_) * P_;
+}
---
+++ multi_sensor_mot/algorithm/kalman_filter/extended_kalman_filter.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <Eigen/Dense>
+
+class ExtendedKalmanFilter {
+  public:
+    ExtendedKalmanFilter() : is_initialized_(false) {}
+
+    ~ExtendedKalmanFilter() {}
+
+    void initialization(Eigen::VectorXd x_in){
+      x_ = x_in;
+      is_initialized_ = true;
+    }
+
+    inline bool isInitialization() {
+      return is_initialized_;
+    }
+
+    void setF(Eigen::MatrixXd F_in){
+      F_ = F_in;
+    }
+    
+    void setP(Eigen::MatrixXd P_in){
+      P_ = P_in;
+    }
+
+    void setQ(Eigen::MatrixXd Q_in){
+      Q_ = Q_in;
+    }
+
+    void setR(Eigen::MatrixXd R_in){
+      R_ = R_in;
+    }
+
+    Eigen::MatrixXd calculateJacobian();
+    void predict();
+    void measurementUpdate(const Eigen::VectorXd& z);
+
+  private:
+    bool is_initialized_;
+    // state vector
+    Eigen::VectorXd x_;
+    // state transistion matrix
+    Eigen::MatrixXd F_;
+    // state convariance matrix
+    Eigen::MatrixXd P_;
+    // process convariance matrix
+    Eigen::MatrixXd Q_;
+    // jacobian convariaance matrix
+    Eigen::MatrixXd H_;
+    // measurement convariance matrix
+    Eigen::MatrixXd R_;
+};
