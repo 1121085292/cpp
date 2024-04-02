a0ad73d3fc9b950abe63227bc05ba8b6a0d63ba4
tang <1121085292@qq.com>
2024-04-02 21:12:38 +0800

    2112

--- effective/main.cc
+++ effective/main.cc
@@ -1,3 +1,4 @@
+#include <iostream>
 // 1.c++联邦：c++中c语言，面相对象（类的封装、继承和多态），c++中的模板，c++中的STL标准库
 // 2.Prefer consts, enums, and inlines to #defines
 /*  对于简单常量，首选const和enum
@@ -18,7 +19,64 @@ inline T CallWithMax(const T& a,const T& b){
 }
 
 // 3.Use const whenever possible
+// 4.确保对象在使用前被初始化
+// 5.了解c++默默编写并调用了哪些函数，类的默认构造，默认析构，拷贝构造等
+// 6.不使用编译器自动生成的函数，需自己重新实现
+// 7.为多态基类声明虚析构函数
+class BaseTimer {
+  public:
+  enum class TimerType {
+    Atomic, water 
+  };
+  public:
+    BaseTimer(){
+      std::cout << "BaseTimer constructor" << std::endl;
+    }
+    virtual ~BaseTimer(){
+      std::cout << "BaseTimer destructor" << std::endl;
+    }
+  private:
+};
+
+class AtomicTimer : public BaseTimer{
+  public:
+    AtomicTimer(){
+      std::cout << "AtomicTimer constructor" << std::endl;
+    }
+    ~AtomicTimer(){
+      std::cout << "AtomicTimer destructor" << std::endl;
+    }
+  private:
+};
+
+
+class WaterTimer : public BaseTimer{
+  public:
+    WaterTimer(){
+      std::cout << "WaterTimer constructor" << std::endl;
+    }
+    ~WaterTimer(){
+      std::cout << "WaterTimer destructor" << std::endl;
+    }
+  private:
+};
+
+BaseTimer* getTimer(BaseTimer::TimerType type){
+  switch (type)
+  {
+  case BaseTimer::TimerType::Atomic:
+    return new AtomicTimer();
+  case BaseTimer::TimerType::water:
+    return new WaterTimer();
+  default:
+    return new BaseTimer();
+  }
+}
 
 int main(){
+  // 7.为多态基类声明虚析构函数
+  // BaseTimer* timer = getTimer(BaseTimer::TimerType::Atomic);
+  // delete timer;
+
   return 0;
 }
